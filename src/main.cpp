1c510855762972807b8c5116548771260194e8e3
wyatt <huckabywyatt@gmail.com>
2024-05-11 09:17:02 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by wyatt on 5/11/24.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+    std::cout << "compiler version: " << __VERSION__ << std::endl;
+    return EXIT_SUCCESS;
+}
